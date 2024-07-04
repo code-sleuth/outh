46427a81676126edcf0ba09f3ff22281cff0b997
Ibrahim Mbaziira <code.ibra@gmail.com>
2024-07-04 05:30:06 +0300

    feat(app-state): add app state
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

---
+++ auth-service/src/app_state.rs
@@ -0,0 +1,32 @@
+/*
+   Copyright 2024 Ibrahim Mbaziira
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+use std::sync::{Arc};
+use tokio::sync::RwLock;
+use crate::domain::UserStore;
+
+pub type UserStoreType = Arc<RwLock<dyn UserStore + Send + Sync>>;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub user_store: UserStoreType,
+}
+
+impl AppState {
+    pub fn new(user_store: UserStoreType) -> Self {
+        Self { user_store }
+    }
+}
