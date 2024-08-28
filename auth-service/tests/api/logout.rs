74dc2e29f05ecd22c2288fae54dcc0af6c39fd29
Ibrahim Mbaziira <code.ibra@gmail.com>
2024-08-28 17:19:12 +0300

    chore: test invalid token
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -138,3 +138,31 @@ async fn should_return_400_if_logout_called_twice_in_a_row() {
         "Missing auth token".to_owned()
     );
 }
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+    // create invalid cookie
+    app.cookie_jar.add_cookie_str(
+        &format!(
+            "{}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
+            JWT_COOKIE_NAME
+        ),
+        &Url::parse("http://127.0.0.1").expect("failed to parse url"),
+    );
+    let response = app.logout().await;
+    assert_eq!(response.status().as_u16(), 401);
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME);
+    assert!(auth_cookie.is_none());
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("could not deserialize response body to ErrorResponse")
+            .error,
+        "Invalid auth token".to_owned()
+    );
+}
