493b612179fb0b83a160e890ffb48b61e237e65c
Ibrahim Mbaziira <code.ibra@gmail.com>
2024-08-28 18:00:25 +0300

    chore: tack on verify token tests
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -14,14 +14,119 @@
    limitations under the License.
 */
 
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
+use auth_service::{utils::constants::JWT_COOKIE_NAME, ErrorResponse};
 
 #[tokio::test]
-#[ignore]
-async fn verify_token() {
+async fn should_return_200_for_valid_token() {
     let app = TestApp::new().await;
-    let body = vec![("token", "jwt")];
-    let response = app.verify_token(&body).await;
+    let random_email = get_random_email();
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "noTsoSecure",
+        "require2FA": false,
+    });
+    let response = app.signup(&signup_body).await;
+    assert_eq!(response.status().as_u16(), 201);
 
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "noTsoSecure",
+    });
+    let response = app.login(&login_body).await;
     assert_eq!(response.status().as_u16(), 200);
+
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+    assert!(!auth_cookie.value().is_empty());
+
+    let token = auth_cookie.value();
+    let verify_token_body = serde_json::json!({
+        "token": token,
+    });
+    let response = app.verify_token(&verify_token_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+    let test_cases = vec!["", "invalid_token"];
+    for test_case in test_cases {
+        let verify_token_body = serde_json::json!({
+            "token": test_case
+        });
+        let response = app.verify_token(&verify_token_body).await;
+        assert_eq!(response.status().as_u16(), 401);
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid auth token".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_401_if_banned_token() {
+    let app = TestApp::new().await;
+    let random_email = get_random_email();
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "notSoSecure",
+        "require2FA": false
+    });
+    let response = app.signup(&signup_body).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "notSoSecure",
+    });
+    let response = app.login(&login_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+    assert!(!auth_cookie.value().is_empty());
+
+    let token = auth_cookie.value();
+    let response = app.logout().await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    let verify_token_body = serde_json::json!({
+        "token": token,
+    });
+    let response = app.verify_token(&verify_token_body).await;
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("could not deserialize response body to ErrorResponse")
+            .error,
+        "Invalid auth token".to_owned()
+    );
+}
+
+#[tokio::test]
+async fn should_return_422_if_malformed_input() {
+    let app = TestApp::new().await;
+    let test_cases = vec![
+        serde_json::json!({
+            "token": true,
+        }),
+        serde_json::json!({}),
+    ];
+
+    for test_case in test_cases {
+        let response = app.verify_token(&test_case).await;
+        assert_eq!(response.status().as_u16(), 422);
+    }
 }
