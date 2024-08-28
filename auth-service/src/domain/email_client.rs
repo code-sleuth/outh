1cfb642260a732fef07548624a22f01e2566ce8d
Ibrahim Mbaziira <code.ibra@gmail.com>
2024-08-28 20:22:24 +0300

    feat: add EmailClient data store
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- auth-service/src/domain/email_client.rs
+++ auth-service/src/domain/email_client.rs
@@ -14,16 +14,14 @@
    limitations under the License.
 */
 
-pub mod data_stores;
-pub mod email;
-pub mod email_client;
-pub mod error;
-pub mod password;
-pub mod user;
+use super::Email;
 
-pub use data_stores::*;
-pub use email::*;
-pub use email_client::*;
-pub use error::*;
-pub use password::*;
-pub use user::*;
+#[async_trait::async_trait]
+pub trait EmailClient {
+    async fn send_email(
+        &self,
+        recipient: &Email,
+        subject: &str,
+        content: &str,
+    ) -> Result<(), String>;
+}
